daaf03525bb52710f4a141e13696940f5df654f5
hisral <46518834+hisral@users.noreply.github.com>
2025-12-16 21:12:32 +0700

    Update layout.ts

--- src/layout.ts
+++ src/layout.ts
@@ -7,6 +7,12 @@ export const Layout = (content: any, title: string, user?: any) => html`
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <title>${title}</title>
+  
+  <!-- FAVICON CUSTOM -->
+  <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.jsdelivr.net/gh/hisral/mycdn@e6d14ea/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.jsdelivr.net/gh/hisral/mycdn@e6d14ea/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.jsdelivr.net/gh/hisral/mycdn@e6d14ea/favicon-16x16.png">
+  
   <script src="https://cdn.tailwindcss.com"></script>
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
   <script src="https://unpkg.com/lucide@latest"></script>
@@ -82,4 +88,3 @@ export const Layout = (content: any, title: string, user?: any) => html`
   <script>lucide.createIcons();</script>
 </body>
 </html>
-`;
